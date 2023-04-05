d82ed8240971dab7e52891c91ab749af08f15874
sucheta2010 <115158213+sucheta2010@users.noreply.github.com>
2023-04-05 10:27:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    recognition.start();
+} 
+
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+
+
+function speak(){
+
+    
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    function speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+
+    setTimeout(function()
+    { 
+        img_id = "selfie";
+        take_snapshot();
+        speak_data = "Taking your next Selfie in 10 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    }, 5000);
+}
+
+  function take_snapshot()
+    {
+        console.log(img_id);
+        
+        Webcam.snap(function(data_uri){
+            if(img_id=="selfie1"){document.getElementsById("result1").innerHtml = '<img id="selfie1" src="'+data_uri+'"/>;
+                                  
+              }
+            if(img_id=="selfie2"){document.getElementsById("result1").innerHtml = '<img id="selfie1" src="'+data_uri+'"/>;
+                                  
+              }
+            if(img_id=="selfie3"){document.getElementsById("result1").innerHtml = '<img id="selfie1" src="'+data_uri+'"/>;
+              }
+          });
